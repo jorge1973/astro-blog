2f80f10fa68739e0cc47e44736b18b0123903a4e
jorge1973 <jediazf@protonmail.com>
2025-02-12 13:10:39 -0500

    second commit

--- src/config/site-config.ts
+++ src/config/site-config.ts
@@ -1,4 +1,4 @@
 export const siteConfig = {
-  title: "Fernando's Blog",
-  description: 'Blog oficial de Fernando Herrera - Web developer',
+  title: "Jorge's Blog",
+  description: 'Blog oficial de Jorge - Web developer',
 };
